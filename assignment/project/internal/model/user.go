3db27be86c5d547dbe5b177c0125ad7e1a27c136
trunglt4 <trunglt4@vmogroup.com>
2025-04-16 10:53:46 +0700

    Modify user model

--- assignment/project/internal/model/user.go
+++ assignment/project/internal/model/user.go
@@ -3,11 +3,16 @@ package model
 // User represents user table from the database
 type User struct {
 	BaseModel
-	FirstName    string
-	LastName     string
-	Email        string
-	Password     string
-	Pseudonym    string
-	ProfileImage string
-	Biography    string
+	FirstName    string `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
+	LastName     string `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
+	Email        string `gorm:"type:varchar(255); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null; uniqueIndex"`
+	Password     string `gorm:"type:varchar(255); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
+	Pseudonym    string `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null; uniqueIndex"`
+	ProfileImage string `gorm:"type:text; CHARACTER SET utf8 COLLATE utf8_unicode_ci"`
+	Biography    string `gorm:"type:text; CHARACTER SET utf8 COLLATE utf8_unicode_ci"`
+	Posts        []Post
+	Comments     []Comment
+	FavoritePost []*Post `gorm:"many2many:favorite_post"`
+	FollowUser   []*User `gorm:"many2many:follow_user;joinForeignKey:UserID;joinReferences:FollowUserID"`
+	Followers    []*User `gorm:"many2many:follow_user;joinForeignKey:FollowUserID;joinReferences:UserID"`
 }
