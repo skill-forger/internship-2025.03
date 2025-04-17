0ccb040beb5b8b21468fdc6160205ca25e6670b0
trunglt4 <trunglt4@vmogroup.com>
2025-04-17 10:50:44 +0700

    Deleted duplicate fields in User

--- assignment/project/migrations/schema/versions/20250416000000.go
+++ assignment/project/migrations/schema/versions/20250416000000.go
@@ -9,24 +9,16 @@ import (
 func Migrate20250416000000(db *gorm.DB) error {
 
 	type User struct {
-		model.BaseModel
-		FirstName    string  `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
-		LastName     string  `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
-		Email        string  `gorm:"type:varchar(255); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null; uniqueIndex"`
-		Password     string  `gorm:"type:varchar(255); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
-		Pseudonym    string  `gorm:"type:varchar(100); CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null; uniqueIndex"`
-		ProfileImage string  `gorm:"type:text; CHARACTER SET utf8 COLLATE utf8_unicode_ci"`
-		Biography    string  `gorm:"type:text; CHARACTER SET utf8 COLLATE utf8_unicode_ci"`
-		IsVerified   bool    `gorm:"type:tinyint(1); default:false"`
-		FollowUser   []*User `gorm:"many2many:follow_user;joinForeignKey:UserID;joinReferences:FollowUserID"`
-		Followers    []*User `gorm:"many2many:follow_user;joinForeignKey:FollowUserID;joinReferences:UserID"`
+		IsVerified bool    `gorm:"type:tinyint(1); default:false"`
+		FollowUser []*User `gorm:"many2many:follow_user;joinForeignKey:UserID;joinReferences:FollowUserID"`
+		Followers  []*User `gorm:"many2many:follow_user;joinForeignKey:FollowUserID;joinReferences:UserID"`
 	}
 
 	type Post struct {
 		model.BaseModel
-		Title      string  `gorm:"type:varchar(255); not null"`
-		Body       string  `gorm:"type:text; not null"`
-		Slug       string  `gorm:"type:varchar(255); not null; unique"`
+		Title      string  `gorm:"type:varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
+		Body       string  `gorm:"type:text CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
+		Slug       string  `gorm:"type:varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null; unique"`
 		IsPublic   bool    `gorm:"type:tinyint(1); default:false"`
 		UserID     uint    `gorm:"type:bigint(11); unsigned; not null"`
 		User       *User   `gorm:"foreignKey:UserID"`
@@ -35,7 +27,7 @@ func Migrate20250416000000(db *gorm.DB) error {
 
 	type Comment struct {
 		model.BaseModel
-		Content         string     `gorm:"type:text; not null"`
+		Content         string     `gorm:"type:text CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
 		PostID          uint       `gorm:"type:bigint(11); unsigned; not null"`
 		UserID          uint       `gorm:"type:bigint(11); unsigned; not null"`
 		ParentCommentID *uint      `gorm:"type:bigint(11); unsigned"`
@@ -47,7 +39,7 @@ func Migrate20250416000000(db *gorm.DB) error {
 
 	type Tag struct {
 		model.BaseModel
-		Name  string  `gorm:"type:varchar(100); not null"`
+		Name  string  `gorm:"type:varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci; not null"`
 		Posts []*Post `gorm:"many2many:post_tag"`
 	}
 
