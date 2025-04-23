62b424678f4cd82af0f9bf4dc5ee9f4a72e2b79e
NamTK-Golang <namtk2@vmogroup.com>
2025-04-23 13:00:34 +0700

    fix model spelling error

--- assignment/project/internal/model/post.go
+++ assignment/project/internal/model/post.go
@@ -3,9 +3,9 @@ package model
 // Post represents post table from the database
 type Post struct {
 	BaseModel
-	Title    string
-	Body     string
-	Slug     string
-	IsPublic bool
-	UserID   int
+	Title       string
+	Body        string
+	Slug        string
+	IsPublished bool
+	UserID      int
 }
